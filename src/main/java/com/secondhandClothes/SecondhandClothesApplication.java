112660300f4bf41382ae74ad075b7e2b3523b7c8
hongminseo <pj.hongminseo@gmail.com>
2025-07-28 23:05:50 +0900

    11

---
+++ src/main/java/com/secondhandClothes/SecondhandClothesApplication.java
@@ -0,0 +1,14 @@
+package com.secondhandClothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondhandClothesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondhandClothesApplication.class, args);
+	}
+	
+	
+}
